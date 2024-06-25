6003d18e3577f9eca0ff8e7ade59a75d9b47e9ba
Ali Zaid <alizaidshaikh9975@gmail.com>
2024-06-26 02:27:31 +0530

    update js code to show more defintions and examples recived from api and add wikipedia link

--- dist/app.js
+++ dist/app.js
@@ -3,75 +3,84 @@ function search() {
   // Redirect to the meaning page with the word as a parameter
   if (searchInput != "") {
     window.location.href = `meaning.html?word=${searchInput}`;
+    document.getElementById("searchInput").value = ''
   } else {
     alert("Enter Word!");
   }
 }
 
+let audioPlayer = document.getElementById("audioPlayer");
+let wordName = document.querySelector("#word-name");
+let meaningContainer = document.querySelector("#meaning-ontainer");
+
 // Extract the word parameter from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const word = urlParams.get("word");
-let audioIcon = document.querySelector("#audio-icon");
 
 // Fetch meaning from API
 fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    let audioPlayer = document.getElementById("audioPlayer");
-    let wordName = document.querySelector("#word-name");
-    let meaningContainer = document.querySelector("#meaning-ontainer");
-
-    try {
-      // Check if the word exists
-      if (data.title === "No Definitions Found") {
-        wordName.innerText = "No definitions found for the word.";
-      } else {
-        // Insert HTML content for the first three meanings
-        wordName.innerHTML = `<div class="pr-3 ml-1 mt-5 w-auto flex flex-row items-center gap-2 overflow-x-scroll">
+    // Check if the word exists
+    if (data.title === "No Definitions Found") {
+      wordName.innerText = "No definitions found for the word.";
+    } else {
+      // Insert HTML content for the first three meanings
+      wordName.innerHTML = `<div class="hello pr-3 ml-1 mt-5 w-auto flex flex-row items-center gap-2 overflow-x-scroll">
         <i class="fa-solid fa-volume-high text-xs text-white bg-blue-700 p-2 rounded-full" id="audio-icon" onclick="playWord()"></i>
         <h1 class="font-extrabold text-3xl tracking-wider md:text-black">${word}</h1>
         <span class="text-2xl font-bold text-gray-700 md:text-slate-700">${data[0].meanings[0].partOfSpeech}</span>
       </div>`;
-
-        let defNum = 0;
-        for (let i = 0; i < 3; i++) {
-          defNum++;
-          const meaning = data[0].meanings[0].definitions[i];
-          meaningContainer.innerHTML += `
+      const meaning = data[0].meanings[0].definitions;
+      const meaningNew = meaning.filter((meanin) => meanin.example || meanin.definition);
+      const wikiUrl = `https://en.wikipedia.org/wiki/${word}`
+      // console.log(meaning)
+      // console.log(meaningNew)
+      let defNum = 0;
+      for (let i = 0; i < meaningNew.length; i++) {
+        defNum++;
+        meaningContainer.innerHTML += `
             <div class="relative bg-slate-200 p-6 rounded-xl shadow-xl md:max-w-[400px] min-w-[300px]">
             <p class="font-extrabold text-blue-700">Defination ${defNum}:</p>
-            <p class="mb-3 text-[1rem] font-semibold tracking-wide">${meaning.definition}</p>
-            <p class="font-extrabold text-blue-700">Example:</p>
-            <p class="text-[1rem] font-semibold tracking-wide">${meaning.example}</p>
+            <p class="mb-3 text-[1rem] font-semibold tracking-wide">${meaningNew[i].definition}</p>
+            <p class="font-extrabold text-blue-700">${meaningNew[i].example ? 'Example:' : ''}</p>
+            <p class="text-[1rem] font-semibold tracking-wide">${meaningNew[i].example ? meaningNew[i].example : ''}</p>
+            <a href="${wikiUrl}" target="_blank" >Wikipedia</a>
             <div class="corner top-left bg-red-500 rounded-tl-xl"></div>
             <div class="corner top-right bg-blue-500 rounded-tr-xl"></div>
             <div class="corner bottom-left bg-green-500 rounded-bl-xl"></div>
             <div class="corner bottom-right bg-yellow-500 rounded-br-xl"></div>
             </div>`;
-        }
-      }
-    } catch {
-      console.log("error");
-    }
-    for (let j = 0; j < 3; j++) {
-      if (data[0].phonetics && data[0].phonetics[j].audio) {
-        const audioUrl = data[0].phonetics[j].audio;
-        console.log("Audio URL:", audioUrl);
-        audioPlayer.src = audioUrl;
-        console.log("Src attribute set:", audioPlayer.src);
-        break;
+            // console.log(`word ${i} added`)
       }
     }
+
+      const audioUrl = data[0].phonetics
+      const audioUrlExist = audioUrl.filter((url) => url.audio)
+      // console.log(audioUrl)
+      // console.log(audioUrlExist.length)
+      if(audioUrlExist.length > 0){
+        audioPlayer.src = audioUrlExist[0].audio;
+      }
   })
-  .catch((error) => console.error("Error:", error));
 
 function playWord() {
   audioPlayer.playbackRate = 0.7;
   audioPlayer.play();
 }
 
+document.addEventListener('keydown' , (e) => {
+  if(e.key === 'Enter'){
+    if(!word){
+      search()
+      document.getElementById("searchInput").value = ''
+    }
+    else{
+      console.log(e.key)
+    }
+  }
+})
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Show the body smoothly after content is loaded
   document.body.classList.remove("opacity-0");
 });
